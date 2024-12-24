2c159487f924a941b7d0b2812e02775e255aa872
fajarriv <rivaldiibnusina@gmail.com>
2024-12-24 20:25:39 +0700

    add fe deployment origins

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  
+    allow_origins=["http://localhost:5173","http://194.233.81.197:5173"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
